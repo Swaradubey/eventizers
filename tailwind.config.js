f6003a5a506e6226fb0d6354357396e1b5ff5515
Your GitHub Username <swara@hexerve.com>
2026-07-08 22:34:06 +0530

    Fix production header navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./invitehub/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./invitehub/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./invitehub/context/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
